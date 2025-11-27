2a4a104d6f7f68acbb06f98e5b3557d63d0fb42d
caio180 <caio.lima.viana2009@gmail.com>
2025-11-27 16:11:20 -0300

    Em breve continuação

---
+++ Quiz.js
@@ -0,0 +1,36 @@
+let questoes = prompt("Digite o número 1 para iniciar: ")
+
+while (questoes == "1") {
+
+        var q1 = prompt("1) Em quantos dias a linguagem java Script foi criada ? \nA) 10 dias. \nB) 20 dias. \nC) 35 dias. \nD) 50 dias. \n")
+        if (q1.toUpperCase() ==="A"){
+            console.log("Parabéns, você acertou")
+        }
+        else {
+            console.log("Alternativa incorreta, tente novamente na próxima.")
+        }
+    
+        var q2 = prompt("2) Quem é o fundador do sistema operacional (linux) ? \nA) Elon Musk. \nB) Bill Gate. \nC) Linus Torvalds. \nD) Paul Allen. \n")
+        if (q2.toUpperCase() === "C") {
+            console.log("Parabéns, você acertou")
+        }
+        else {
+            console.log("Alternativa incorreta, tente novamente na próxima.")
+        }    
+        var q3 = prompt("3) Quem é considerado o pai da informática ? \nA) Bill Gates. \nB) Alan Turing. \nC) Luiz Fernando (My Angel). \nD) Steve Jobs. \n")
+        if (q3.toUpperCase() === "B") {
+            console.log("Parabéns, você acertou")
+        }
+        else {
+            console.log("Alternativa incorreta, tente novamente na próxima.")
+        }
+        var q4 = prompt("4) O que é um (pixel) ? \nA) Uma linguagem de programação para gráficos. \nB) Um tipo de memória de computador. \nC) O menor elemento em uma imagem digital. \nD) Um formato de arquivo de áudio. \n")
+        if (q4.toUpperCase() === "C") {
+            console.log("Parabéns, você acertou")
+        }
+        else {
+            console.log("Alternativa incorreta, tente novamente na próxima.")
+        }
+
+
+}
