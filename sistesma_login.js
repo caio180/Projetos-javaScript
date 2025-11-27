d257494820f2dd395baefca887872d23b104c6ed
caio180 <caio.lima.viana2009@gmail.com>
2025-11-27 15:11:49 -0300

    Atualização

---
+++ sistesma_login.js
@@ -0,0 +1,11 @@
+let email_correto = "teste.testando@gmail.com"
+let senha_correta = "1234"
+let email = prompt("Digite seu email: ")
+let senha = prompt("Digite sua senha: ")
+
+if (email == email_corret && senha == senha_correta) {
+    console.log("Login bem sucedido")
+}
+else {
+    console.log("Login incorreto")
+}
