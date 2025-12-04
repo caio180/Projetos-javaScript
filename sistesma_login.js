6db823ab02dc5e40aa90baa94fd709eb572982c9
caio180 <caio.lima.viana2009@gmail.com>
2025-12-04 19:44:27 -0300

    Testes

--- sistesma_login.js
+++ sistesma_login.js
@@ -1,11 +1,6 @@
-let email_correto = "teste.testando@gmail.com"
-let senha_correta = "1234"
-let email = prompt("Digite seu email: ")
-let senha = prompt("Digite sua senha: ")
+opcoes_cadrastos = prompt("Digite a opção desejada: \n1) Realizar seu cadrastro \n2) Já sou cadastrado \n")
+email = prompt("Digite seu email: ")
+senha =  prompt("Digite sua senha: ")
 
-if (email == email_corret && senha == senha_correta) {
-    console.log("Login bem sucedido")
-}
-else {
-    console.log("Login incorreto")
-}
+
+
